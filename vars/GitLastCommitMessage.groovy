565246fa2481440bf33024030ffc5372afeca3dd
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-22 21:14:06 +0200

    +

--- vars/GitLastCommitMessage.groovy
+++ vars/GitLastCommitMessage.groovy
@@ -1,13 +1,26 @@
 def call() {
+  def useShell = sh
   try {
-    return sh(
-      script: "git log -1 --oneline --pretty=\"format:%s\"",
-      returnStdout: true
-    ).trim()
+    sh "echo"
   } catch (Exception ex) {
-    return powershell(
-      script: "git log -1 --oneline --pretty=\"format:%s\"",
-      returnStdout: true
-    ).trim()
+    useShell = powershell
   }
+
+  return useShell(
+    script: "git log -1 --oneline --pretty=\"format:%s\"",
+    returnStdout: true
+  ).trim()
+
+
+  // try {
+  //   return sh(
+  //     script: "git log -1 --oneline --pretty=\"format:%s\"",
+  //     returnStdout: true
+  //   ).trim()
+  // } catch (Exception ex) {
+  //   return powershell(
+  //     script: "git log -1 --oneline --pretty=\"format:%s\"",
+  //     returnStdout: true
+  //   ).trim()
+  // }
 }
