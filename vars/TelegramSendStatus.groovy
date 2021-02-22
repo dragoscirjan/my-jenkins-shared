414fa7894a0051ca9f4f4f43b804f2d758252f4c
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-22 04:04:22 +0200

    +

--- vars/TelegramSendStatus.groovy
+++ vars/TelegramSendStatus.groovy
@@ -1,15 +1,16 @@
 
 def call(def status, def token, def chatId) {
-    def message = """-------------------------------------
-### Jenkins build: *${status.toUpperCase()}!*
+    def message = """
+Jenkins build: *${status.toUpperCase()}!*
+------------------
 
 > Repository:    ${env.JOB_NAME}
 Branch:        ${env.BRANCH_NAME}
-*Commit Msg:*\
+*Commit Msg:*
 ...TODO:
 
 > [Job Log here](${env.BUILD_URL}/consoleText)
---------------------------------------"""
+"""
 
     TelegramSend(message, token, chatId)
 }
