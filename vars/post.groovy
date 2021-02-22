6a775daf6ab225bb0a35e27c9c6d31e3a397852c
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-22 02:43:15 +0200

    +

--- vars/post.groovy
+++ vars/post.groovy
@@ -1,11 +1,12 @@
 
 
 def TelegramSendStatusOK(token, chatId) {
-    sh """
-        curl -s -X POST https://api.telegram.org/bot${token}/sendMessage \
-            -d chat_id=${chatId} \
-            -d parse_mode="Markdown" \
-            -d text="\
+    script{
+        sh """
+            curl -s -X POST https://api.telegram.org/bot${token}/sendMessage \
+                -d chat_id=${chatId} \
+                -d parse_mode="Markdown" \
+                -d text="\
 -------------------------------------\n\
 Jenkins build *OK!*\n\
 Repository:  ${env.JOB_NAME}\n\
@@ -15,5 +16,6 @@ Branch:      ${env.BRANCH_NAME}\n\
 [Job Log here](${env.BUILD_URL}/consoleText)\n\
 --------------------------------------\n\
 "
-    """
+        """
+    }
 }
