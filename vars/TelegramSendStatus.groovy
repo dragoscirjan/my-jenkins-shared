59b246255da980fab4038f841df68d0a52e1e268
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-02-22 03:58:29 +0200

    +

--- vars/TelegramSendStatus.groovy
+++ vars/TelegramSendStatus.groovy
@@ -1,20 +1,14 @@
 
 def call(def status, def token, def chatId) {
-    if (status.toLowerCase() == 'ok') {
-        status = "<span style='color: green;'>${status}</status>"
-    }
-    if (status.toLowerCase() == 'failed') {
-        status = "<span style='color: red;'>${status}</status>"
-    }
-
-
-    def message = """-------------------------------------\n\
-Jenkins build *${status}!*\n\
-Repository:  ${env.JOB_NAME}\n\
-Branch:      ${env.BRANCH_NAME}\n\
+    def message = """-------------------------------------\n
+Jenkins build: *${status.toUpperCase()}!*\n
+-------------------------------------\n
+Repository:    ${env.JOB_NAME}\n
+Branch:        ${env.BRANCH_NAME}\n
 *Commit Msg:*\n\
-...TODO:\n\
-[Job Log here](${env.BUILD_URL}/consoleText)\n\
+...TODO:\n
+
+[Job Log here](${env.BUILD_URL}/consoleText)\n
 --------------------------------------"""
 
     TelegramSend(message, token, chatId)
