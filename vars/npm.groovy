5f4f1f757d57e54555246847627a86c14775b537
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-06-19 00:59:44 +0300

    refactor: updates on how npm.release should work

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -1,14 +1,18 @@
-
 /**
  * This method should help you run npm (yarn/pnpm) install and adding a cache layer.
  *
- * @param manager     Node manager to use for installing pakcages. Default: 'npm'.
- * @param cache       Whether to use node_modules cache or not. Default: true.
- * @param cacheKey    Cache unique key to be used in the cache file name. Default: ''.
- * @param useNvm      Whether to use nvm or not. Default: false.
+ * @param manager Node package manager to use for installing packages. Default: 'npm'. (Deprecated. Use `packageManager`)
+ * @param cache Whether to use node_modules cache or not. Default: true.
+ * @param cacheKey Cache unique key to be used in the cache file name. Default: ''.
+ * @param useNvm Whether to use nvm or not. Default: false.
  * @param nodeVersion Nodejs version to use for nvm.runSh() method.
+ * @param packageManager Node package manager to use for installing packages. Default: 'npm'. (Overwrites `manager` which is deprecated.)
  */
 def install(Map options) {
+  if (options.packageManager) {
+    options.manager = options.packageManager
+  }
+
   if (!options.manager) {
     options.manager = 'npm'
   }
@@ -23,7 +27,7 @@ def install(Map options) {
 
   command = "${options.manager} install"
 
-  jobName=env.JOB_NAME.replaceAll(/[^\w]/, '_')
+  jobName = env.JOB_NAME.replaceAll(/[^\w]/, '_')
 
   if (options.cache) {
     try {
@@ -86,125 +90,114 @@ Compress-Archive -Path .\\node_modules -DestinationPath \"\$archivePath\"
  * message for marking the release should have the following form: chore: release v${version}";
  * otherwise you will generate an endless pipeline loop.
  *
- * @param gitCredentialsId     Credentials ID for github/gitlab, if you're publishing to npmjs.com
- * @param gitUrl               Git URL no http(s):// included.
+ * @param gitCredentialsId Credentials ID for github/gitlab, if you're publishing to npmjs.com
+ * @param gitUrl Git URL no http(s):// included.
  *                             i.e. 'github.com/dragoscirjan/my-jenkins-shared.git'
- * @param gitMessageIncludes   Default 'chore: release v'. If this message is met in the commit message, release
+ * @param gitMessageIncludes Default 'chore: release v'. If this message is met in the commit message, release
  *                             command will abort, considering release has been performed already.
  * @param npmTokenCredentialId Default null. Credentials ID for npmjs.com token, if you're publishing to npmjs.com
- * @param noNpmPublish         Default false. Ignore the npm publish command
- * @param packageManager       Default 'npm'. Package manager to use.
- * @param releaseManager       Default 'release-it'. Release manager to use.
- * @param releaseItArgs        Default 'patch'. release-it command arguments. See release-it documentation.
- * @param useNvm               Default: false. Whether to use nvm or not.
- * @param nodeVersion          Default null. Nodejs version to use for nvm.runSh() method.
+ * @param noNpmPublish Default false. Ignore the npm publish command
+ * @param packageManager Default 'npm'. Package manager to use.
+ * @param releaseManager Default 'release-it'. Release manager to use.
+ * @param releaseItArgs Default 'patch'. release-it command arguments. See release-it documentation.
+ * @param useNvm Default: false. Whether to use nvm or not.
+ * @param nodeVersion Default null. Nodejs version to use for nvm.runSh() method.
  *
  * Example of realease-it.json config:
- * <code>
- * {
- *   "git": {
- *     "commitMessage": "chore: release v${version}",
- *     "requireUpstream": false,
- *     "commitArgs": "--no-verify",
- *     "--requireBranch": "master"
- *   },
- *   "plugins": {
- *     "@release-it/conventional-changelog": {
- *       "preset": "angular",
- *       "infile": "CHANGELOG.md"
- *     }
- *   }
- * }
- * </code>
+ * <code>{ "git": { "commitMessage": "chore: release v${version}",
+ "requireUpstream": false,
+ "commitArgs": "--no-verify",
+ "--requireBranch": "master"},
+ "plugins": { "@release-it/conventional-changelog": { "preset": "angular",
+ "infile": "CHANGELOG.md" }}} </code>
  */
-def release(Map args) {
+def release(Map options) {
+  if (!options.gitUrl) {
+    throw new Exception('gitUrl not mentioned')
+  }
 
-    if (!args.gitUrl) {
-        throw new Exception('gitUrl not mentioned')
-    }
+  if (!options.gitCredentialsId) {
+    throw new Exception('gitCredentialsId not mentioned')
+  }
 
-    if (!args.gitCredentialsId) {
-        throw new Exception('gitCredentialsId not mentioned')
-    }
+  if (!options.gitMessageIncludes) {
+    options.gitMessageIncludes = "chore: release v"
+  }
 
-    if (!args.gitMessageIncludes) {
-      args.gitMessageIncludes = "chore: release v"
-    }
+  def commitMessage = git.lastCommitMessage()
+  if (commitMessage.indexOf(options.gitMessageIncludes) >= 0) {
+    return
+  }
 
-    def commitMessage = git.lastCommitMessage()
-    if (commitMessage.indexOf(args.gitMessageIncludes) >= 0) {
-        return
-    }
+  def packageManager = "${options.packageManager ? options.packageManager : 'npm'}"
+  def releaseManager = options.releaseManager ? options.releaseManager : 'release-it'
 
-    def packageManager = "${args.packageManager ? args.packageManager : 'npm'}"
-    def releaseManager = args.releaseManager ? args.releaseManager : 'release-it'
+  def command = """${options.preCommand ? options.preCommand : ''}"""
 
-    def command = """${args.preCommand ? args.preCommand : ''}"""
-
-    if (args.npmTokenCredentialId) {
-        withCredentials([
-            string(credentialsId: npmTokenCredentialId, variable: 'NPMJS_AUTH_TOKEN')
-        ]) {
-            command = """
+  if (options.npmTokenCredentialId) {
+    withCredentials([
+      string(credentialsId: npmTokenCredentialId, variable: 'NPMJS_AUTH_TOKEN')
+    ]) {
+      command = """
 ${command}
 
 npm config set registry http://registry.npmjs.com
 npm set //registry.npmjs.com/:_authToken ${NPMJS_AUTH_TOKEN}
 """
-        }
     }
+  }
 
-    withCredentials([usernamePassword(
-        credentialsId: args.gitCredentialsId,
-        usernameVariable: 'GIT_USER',
-        passwordVariable: 'GIT_TOKEN'
-    )]) {
-        command = """
+  withCredentials([usernamePassword(
+    credentialsId: options.gitCredentialsId,
+    usernameVariable: 'GIT_USER',
+    passwordVariable: 'GIT_TOKEN'
+  )]) {
+    command = """
 ${command}
 
 git remote rm origin;
-git remote add origin https://${GIT_USER}:${GIT_TOKEN}@${args.gitUrl};
+git remote add origin https://${GIT_USER}:${GIT_TOKEN}@${options.gitUrl};
 git fetch;
 git checkout .;
 git checkout ${env.BRANCH_NAME};
 git pull origin ${env.BRANCH_NAME};
 git status;
 """
-    }
+  }
 
-    command = """
+  command = """
 ${command}
 
 ${packageManager} install;
 """
 
-    if (releaseManager == 'release-it') {
-        def releaseItArgs = args.releaseItArgs ? args.releaseItArgs : 'patch'
+  if (releaseManager == 'release-it') {
+    def releaseItArgs = options.releaseItArgs ? options.releaseItArgs : 'patch'
 
-        command = """
+    command = """
 ${command}
 
 ${packageManager} run release -- ${releaseItArgs};
 """
-    }
+  }
 
-    if (!args.noNpmPublish) {
-        command = """
+  if (!options.noNpmPublish) {
+    command = """
 ${command}
 
 npm publish;
 """
-    }
+  }
 
-    if (args.debug) {
-      echo command
+  if (options.debug) {
+    echo command
 
-      return
-    }
+    return
+  }
 
-  if (!args.useNvm) {
+  if (!options.useNvm) {
     utils.runSh command
   } else {
-    nvm.runSh command, args.nodeVersion
+    nvm.runSh command, options.nodeVersion
   }
 }
